f14dc2af29dccd4f1058262fd4ffde9488f11cd0
AIR <air@etu-189-5.vpn-inp.grenoble-inp.fr>
2024-10-08 13:06:20 +0200

    final

--- resources/asg_web_sg.tf
+++ resources/asg_web_sg.tf
@@ -4,26 +4,20 @@ resource "aws_security_group" "web-asg-sg" {
     vpc_id      = aws_vpc.my-vpc.id
 
 
-    # ingress {
-    #     from_port   = 80
-    #     to_port     = 80
-    #     protocol    = "tcp"
-    #     security_groups = [aws_security_group.lb-web-sg.id]
-    # }
-
-    # ingress {
-    # from_port   = 22
-    # to_port     = 22
-    # protocol    = "tcp"
-    # cidr_blocks = ["0.0.0.0/0"]
-    # }
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        security_groups = [aws_security_group.lb-web-sg.id]
+    }
 
     ingress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     }
+
 
     egress {
         from_port = 0
--- resources/lb_app_sg.tf
+++ resources/lb_app_sg.tf
@@ -1,17 +1,11 @@
 resource "aws_security_group" "lb-app-sg" {    
     vpc_id      = aws_vpc.my-vpc.id
     
-    # ingress {
-    #     from_port   = 80
-    #     to_port     = 80
-    #     protocol    = "tcp"
-    #     security_groups = [aws_security_group.web-asg-sg.id]
-    # }
-     ingress {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        security_groups = [aws_security_group.web-asg-sg.id]
     }
 
     egress {
