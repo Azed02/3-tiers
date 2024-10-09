1c3112ab49b699463cbcc24a65ece1b87b1bd307
AIR <air@etu-189-150.vpn-inp.grenoble-inp.fr>
2024-10-09 11:05:47 +0200

    final set

--- resources/db_subnet_group.tf
+++ resources/db_subnet_group.tf
@@ -1,9 +1,9 @@
-# resource "aws_db_subnet_group" "db-sub-group" {
-#     name       = "db-sub-group"
-#     subnet_ids = [aws_subnet.db-sub1.id , aws_subnet.db-sub2.id]
+resource "aws_db_subnet_group" "db-sub-group" {
+    name       = "db-sub-group"
+    subnet_ids = [aws_subnet.db-sub1.id , aws_subnet.db-sub2.id]
 
-#     tags = {
-#         Name = "db-sub-group"
-#     }
+    tags = {
+        Name = "db-sub-group"
+    }
   
-# }
+}
